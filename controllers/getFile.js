e837336efc9a2a7bfb778287f1853ba741abe3a5
lilleman <lilleman@larvit.se>
2016-09-10 16:12:15 +0200

    Actually added the controller

---
+++ controllers/getFile.js
@@ -0,0 +1,20 @@
+'use strict';
+
+const	lfs	= require('larvitfs'),
+	notFoundPath	= lfs.getPathSync('controllers/404.js'),
+	lFiles	= require(__dirname + '/../index.js');
+
+exports.run = function(req, res, cb) {
+	const file = new lFiles.File({'slug': req.urlParsed.pathname}, function(err) {
+		if (err) { cb(err, req, res, {}); return; }
+
+		if (file.uuid === undefined) {
+			// 404!!!
+			require(notFoundPath).run(req, res, cb);
+			return;
+		}
+
+		res.writeHead(200, 'application/octet-stream');
+		res.end(file.data);
+	});
+};
