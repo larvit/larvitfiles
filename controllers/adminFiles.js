1027b5180e7aacb2e8cb5af999e92f0026768dee
lilleman <lilleman@larvit.se>
2016-09-10 17:49:01 +0200

    Added admin pages

---
+++ controllers/adminFiles.js
@@ -0,0 +1,15 @@
+'use strict';
+
+const	lFiles	= require(__dirname + '/../index.js');
+
+exports.run = function(req, res, cb) {
+	const	files	= new lFiles.Files(),
+		data	= {'global': res.globalData};
+
+	data.global.menuControllerName = 'files';
+
+	files.get(function(err, result) {
+		data.files = result;
+		cb(err, req, res, data);
+	});
+};
