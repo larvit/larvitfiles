abdc67662bbbe6468494f87fbeec9f619faaa5e5
lilleman <lilleman@larvit.se>
2016-09-11 16:24:20 +0200

    Working larvitadmingui admin pages

--- controllers/adminFileEdit.js
+++ controllers/adminFileEdit.js
@@ -10,7 +10,8 @@ exports.run = function(req, res, cb) {
 
 	let file;
 
-	data.global.menuControllerName = 'adminFiles';
+	data.global.menuControllerName	= 'adminFiles';
+	data.global.errors	= [];
 
 	if (data.global.urlParsed.query.uuid !== undefined) {
 		tasks.push(function(cb) {
@@ -19,8 +20,12 @@ exports.run = function(req, res, cb) {
 	}
 
 	if (data.global.formFields.save !== undefined) {
-		const newFileData = {'slug': data.global.formFields.slug, 'metadata': {}};
+		const	newFileData	= {
+					'slug':	data.global.formFields.slug,
+					'metadata':	{}
+				};
 
+		// Set metadata
 		for (let i = 0; data.global.formFields.metaDataName[i] !== undefined; i ++) {
 			if (data.global.formFields.metaDataName[i] !== '' && data.global.formFields.metaDataValue[i] !== '') {
 				const	name	= data.global.formFields.metaDataName[i],
@@ -34,6 +39,24 @@ exports.run = function(req, res, cb) {
 			}
 		}
 
+		// Check so slug is not taken
+		tasks.push(function(cb) {
+			if ((file !== undefined && file.slug !== newFileData.slug) || file === undefined) {
+				lFiles.getFileUuidBySlug(newFileData.slug, function(err, result) {
+					if (err) { cb(err); return; }
+
+					if ((result !== false && file !== undefined && file.uuid !== result) || (file === undefined && result !== false)) {
+						data.global.errors = ['Slug already taken'];
+					}
+
+					cb();
+				});
+			} else {
+				cb();
+			}
+		});
+
+		// Set file data
 		if (req.formFiles !== undefined && req.formFiles.fileData !== undefined && req.formFiles.fileData.size) {
 			tasks.push(function(cb) {
 				fs.readFile(req.formFiles.fileData.path, function(err, fileData) {
@@ -55,26 +78,76 @@ exports.run = function(req, res, cb) {
 			}
 
 			file.slug	= newFileData.slug;
-			file.data	= newFileData.data;
 			file.metadata	= newFileData.metadata;
+
+			if (newFileData.data) {
+				file.data	= newFileData.data;
+			}
 
 			cb();
 		});
 
 		tasks.push(function(cb) {
+			if (data.global.errors.length !== 0) {
+				cb();
+				return;
+			}
+
 			file.save(cb);
 		});
 
 		tasks.push(function(cb) {
-			if (file.uuid !== undefined && data.global.urlParsed.query.uuid === undefined) {
+			if (data.global.errors.length !== 0) {
+				// Do nothing
+			} else if (file.uuid !== undefined && data.global.urlParsed.query.uuid === undefined) {
 				req.session.data.nextCallData	= {'global': {'messages': ['New file created']}};
 				res.statusCode	= 302;
-				res.setHeader('Location', '/adminFileEdit?uuid=' + file.data.uuid);
+				res.setHeader('Location', '/adminFileEdit?uuid=' + file.uuid);
 			} else {
 				data.global.messages = ['Saved'];
 			}
+			cb();
 		});
 	}
+
+	if (data.global.formFields.delete !== undefined) {
+		tasks.push(function(cb) {
+			if (file === undefined) {
+				cb();
+				return;
+			}
+
+			file.rm(function(err) {
+				if (err) { cb(err); return; }
+
+				req.session.data.nextCallData	= {'global': {'messages': ['Movie deleted']}};
+				res.statusCode	= 302;
+				res.setHeader('Location', '/adminFiles');
+				cb();
+			});
+		});
+	}
+
+	// Load saved data to formfields
+	tasks.push(function(cb) {
+		if (file === undefined) {
+			cb();
+			return;
+		}
+
+		data.global.formFields.slug	= file.slug;
+		data.global.formFields.metaDataName	= [];
+		data.global.formFields.metaDataValue	= [];
+
+		for (const key of Object.keys(file.metadata)) {
+			for (let i = 0; file.metadata[key][i] !== undefined; i ++) {
+				data.global.formFields.metaDataName.push(key);
+				data.global.formFields.metaDataValue.push(file.metadata[key][i]);
+			}
+		}
+
+		cb();
+	});
 
 	async.series(tasks, function(err) {
 		cb(err, req, res, data);
