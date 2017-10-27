a819fdda0a22f0f9f5a2facc46c47693c4a3231d
lilleman <lilleman@larvit.se>
2017-10-27 15:00:09 +0200

    Fixed problem with migration #2

--- dbmigration/2.js
+++ dbmigration/2.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const	logPrefix	= 'larvitfiles: ./dbmigration/2.js: ',
-	lFiles	= require(__dirname + '/../index.js'),
+	filesLib	= require(__dirname + '/../index.js'),
 	lUtils	= require('larvitutils'),
 	async	= require('async'),
 	log	= require('winston'),
@@ -11,44 +11,44 @@ exports = module.exports = function (cb) {
 	const	tasks	= [],
 		db	= this.options.dbDriver;
 
-	let files;
+	let	files;
 
-	if (lFiles.storagePath === null) {
-		const e = new Error('storagePath not set on larvitfiles');
-		log.warn(logPrefix + e.message);
-		throw e;
+	if (filesLib.storagePath === null) {
+		const	err	= new Error('storagePath not set on larvitfiles');
+		log.warn(logPrefix + err.message);
+		throw err;
 	}
 
-	if ( ! fs.existsSync(lFiles.storagePath)) {
+	if ( ! fs.existsSync(filesLib.storagePath)) {
 		tasks.push(function (cb) {
-			log.info(logPrefix + 'storagePath "' + lFiles.storagePath + '" does not exist, creating');
-			fs.mkdir(lFiles.storagePath, cb);
+			log.info(logPrefix + 'storagePath "' + filesLib.storagePath + '" does not exist, creating');
+			fs.mkdir(filesLib.storagePath, cb);
 		});
 	}
 
-	// get list of slugs and uuids
+	// Get list of slugs and uuids
 	tasks.push(function (cb) {
 		db.query('SELECT uuid, slug FROM larvitfiles_files', function (err, rows) {
-			files = rows;
+			files	= rows;
 			cb(err);
 		});
 	});
 
-	// write files to disk and save type in db
+	// Write files to disk and save type in db
 	tasks.push(function (cb) {
 		const tasks = [];
 
 		for (const file of files) {
 			tasks.push(function (cb) {
-				db.query('SELECT data FROM larvitfiles_files WHERE uuid = ?', [lUtils.uuidToBuffer(file.uuid)], function (err, result) {
+				db.query('SELECT data FROM larvitfiles_files WHERE uuid = ?', file.uuid, function (err, result) {
 					if (err) return cb(err);
 
 					if (result.length === 0) {
-						log.warn(logPrefix + 'Could not find file with uuid "' + lUtils.formatUuid(file.uuid) + '"');
+						log.warn(logPrefix + 'Could not find file with uuid "' + Utils.formatUuid(file.uuid) + '"');
 						return cb();
 					}
 
-					fs.writeFile(lFiles.storagePath + '/' + lUtils.formatUuid(file.uuid), result[0].data, cb);
+					fs.writeFile(filesLib.storagePath + '/' + lUtils.formatUuid(file.uuid), result[0].data, cb);
 				});
 			});
 		}
